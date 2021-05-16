bb6342121c5767c6c5676cd96be60673e81614f1
weiblejason <weiblejason@gmail.com>
2021-05-16 09:16:56 -0400

    Initial commit

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val name = "Madrigal"
+    val healthPoints = 100
+
+    if (healthPoints == 100) {
+        println(name + " is in excellent condition!")
+    } else {
+        println(name + " is in awful condition!")
+    }
+}
